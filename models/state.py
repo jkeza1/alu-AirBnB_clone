0fc20162b5607ecb69ec8bc07eb2c2d33fa7815d
jkeza1 <j.keza1@alustudent.com>
2024-05-09 15:25:50 +0200

    added state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
